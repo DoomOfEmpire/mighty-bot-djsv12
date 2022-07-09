a79e9bede7fc4b4b4a644641bb4540c4cb81a5f7
DoomOfEmpire <doomofempiret@gmail.com>
2022-07-09 19:22:09 +0300

    scos token

--- main/index.js
+++ main/index.js
@@ -3,4 +3,4 @@ var Constants = Discord.Constants;
 Constants.DefaultOptions.ws.properties.$browser = "Discord Android";
 
 // -> Login
-bot.login(`ODM2OTMyMDY5MzUwNDQwOTgx.GBW0k1.au2n3BFB9MuO-XYtWqRiiQn09Xt5fibGsaFMtM`);
+bot.login(`muie`);
