504068254ea04f7567f97f49aecc7757aa46fee2
DoomOfEmpire <doomofempiret@gmail.com>
2022-07-09 19:18:36 +0300

    mata

--- main/index.js
+++ main/index.js
@@ -3,4 +3,4 @@ var Constants = Discord.Constants;
 Constants.DefaultOptions.ws.properties.$browser = "Discord iOS";
 
 // -> Login
-bot.login(`token`);
+bot.login(`ODM2OTMyMDY5MzUwNDQwOTgx.GBW0k1.au2n3BFB9MuO-XYtWqRiiQn09Xt5fibGsaFMtM`);
