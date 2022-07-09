b01bfa0f6c9eef8066171926463a31960f0aded8
DoomOfEmpire <doomofempiret@gmail.com>
2022-07-09 19:21:33 +0300

    muie

--- main/index.js
+++ main/index.js
@@ -1,6 +1,6 @@
 // -> Phone Status
 var Constants = Discord.Constants;
-Constants.DefaultOptions.ws.properties.$browser = "Discord iOS";
+Constants.DefaultOptions.ws.properties.$browser = "Discord Android";
 
 // -> Login
 bot.login(`ODM2OTMyMDY5MzUwNDQwOTgx.GBW0k1.au2n3BFB9MuO-XYtWqRiiQn09Xt5fibGsaFMtM`);
